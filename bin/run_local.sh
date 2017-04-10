d32fa5df462b2c1ba04edf66e3d97f132e167cdd
Aaron Bieber <aaron@aaronbieber.com>
2017-04-10 19:29:43 -0400

    Convert from uWSGI to pure Flask debug server
    Fixes #1

--- bin/run_local.sh
+++ bin/run_local.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-if [ -z "$VENV" ]; then
-  echo "Please provide the path to your virtualenv in VENV." >&2
-  exit 1
-fi
-
-echo "Using virtualenv in ${VENV}."
-
-if [ ! -x "${VENV}/bin/uwsgi" ]; then
-  echo "Couln't find uwsgi executable in ${VENV}/bin!" >&2
-  echo "Install all requirements with `pip install -r requirements.txt`"
-fi
-
 FLASK_CONFIG="copleyescalators.settings.local.LocalConfig" \
-            ${VENV}/bin/uwsgi \
-            --http :8080 \
-            -H ${VENV} \
-            --py-autoreload 1 \
-            --mount /=server:app
+  FLASK_DEBUG=1 \
+  FLASK_APP=server.py flask run
