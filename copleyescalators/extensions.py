e7eb905cc576648f28e59ab80e8b3d6bab9bf662
Aaron Bieber <aaron@aaronbieber.com>
2017-04-10 19:30:35 -0400

    Add date_string helper for added/updated dates
    This will ensure that we always use the same date string format in
    sqlite, since it has no first-class date datatype.

--- copleyescalators/extensions.py
+++ copleyescalators/extensions.py
@@ -1,2 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+
+def date_string(dt=None):
+    from datetime import datetime
+
+    if dt is None:
+        return datetime.today().strftime("%Y-%m-%d %H:%M:%S.000")
+    else:
+        return dt.strftime("%Y-%m-%d %H:%M:%S.000")
