a53bd88444156f001d8e71b311848a41e30cf9f7
Aaron Bieber <aaron@aaronbieber.com>
2017-04-15 17:13:26 -0400

    Let sqlite find the DB file via relative path
    This removes the somewhat kludgy project_root module and fixes #4.

---
+++ bin/init_db.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sqlite3 copleyescalators/data/escalators.db < copleyescalators/data/schema.sql
--- copleyescalators/settings/local.py
+++ copleyescalators/settings/local.py
@@ -6,10 +6,9 @@ you want but Flask will only see the capitalized ones.
 
 
 from .base import BaseConfig
-from ..project_root import path as project_root
 
 
 class LocalConfig(BaseConfig):
     """Configuration for running in a local development environment.
     """
-    SQLALCHEMY_DATABASE_URI = "sqlite:///%s/data/escalators.db" % project_root
+    SQLALCHEMY_DATABASE_URI = "sqlite:///data/escalators.db"
